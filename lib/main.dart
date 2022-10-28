9fd19232f5b7f5e5ccdf9552d8fe3f5cd80db6c3
GabrieleSacramento <gabisacramentoti@gmail.com>
2022-10-28 19:24:12 -0300

    Initial detail

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App Tarefas',
+      title: 'App Tarefas - Detalhes',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
