4edb2243f366e0cb02f5422d030953983085127d
GabrieleSacramento <gabisacramentoti@gmail.com>
2022-11-09 22:18:33 -0300

    Navigation Between Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:testando/screens/initial_screen.dart';
+=======
+import 'package:tasks/screens/form_screen.dart';
+import 'package:tasks/screens/initial_screen.dart';
+>>>>>>> Stashed changes
 
 void main() {
   runApp(const MyApp());
--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:testando/components/task.dart';
+=======
+import 'package:tasks/components/task.dart';
+import 'package:tasks/screens/form_screen.dart';
+>>>>>>> Stashed changes
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -9,8 +14,6 @@ class InitialScreen extends StatefulWidget {
 }
 
 class _InitialScreenState extends State<InitialScreen> {
-  bool opacidade = true;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,30 +21,32 @@ class _InitialScreenState extends State<InitialScreen> {
         leading: Container(),
         title: const Text('Tarefas'),
       ),
-      body: AnimatedOpacity(
-        opacity: opacidade ? 1 : 0,
-        duration: const Duration(milliseconds: 200),
-        child: ListView(
-          children: const [
-            Task('Aprender flutter', 'assets/images/flutter.png', 3),
-            Task('Aprender Dart', 'assets/images/dart.png', 4),
-            Task('Aprender HTML', 'assets/images/html.png', 3),
-            Task('Aprender Figma', 'assets/images/figma.png', 2),
-            Task('Criar Interface', 'assets/images/ui_2.png', 2),
-            Task('Criando App com flutter', 'assets/images/flutter_2.png', 3),
-            Task('Aprender phyton', 'assets/images/Python.png', 4),
-            Task('Ux para iniciantes', 'assets/images/ux.png', 3),
-            Task('Design system', 'assets/images/design_system_2.png', 4),
-          ],
-        ),
+      body: ListView(
+        children: const [
+          Task('Aprender flutter', 'assets/images/flutter.png', 3),
+          Task('Aprender Dart', 'assets/images/dart.png', 4),
+          Task('Aprender HTML', 'assets/images/html.png', 3),
+          Task('Aprender Figma', 'assets/images/figma.png', 2),
+          Task('Criar Interface', 'assets/images/ui_2.png', 2),
+          Task('Criando App com flutter', 'assets/images/flutter_2.png', 3),
+          Task('Aprender phyton', 'assets/images/Python.png', 4),
+          Task('Ux para iniciantes', 'assets/images/ux.png', 3),
+          Task('Design system', 'assets/images/design_system_2.png', 4),
+          SizedBox(
+            height: 80,
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            opacidade = !opacidade;
-          });
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const FormScreen(),
+            ),
+          );
         },
-        child: const Icon(Icons.remove_red_eye),
+        child: const Icon(Icons.add),
       ),
     );
   }
